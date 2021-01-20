9bbe9965ccb5653693c832fc5a5d72a861c7d063
henry_tujia <1319670017@qq.com>
2021-01-20 18:51:56 +0800

    LSDL

--- PRML/main.py
+++ PRML/main.py
@@ -2,6 +2,35 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from urllib.parse import urlparse
+
+import requests
+
+
+def login_wifi(stuid, password):
+    try:
+        query_string = urlparse(requests.get("http://210.77.16.21").url).query
+        payload = {
+            "userId": stuid,
+            "password": password,
+            "service": "",
+            "queryString": query_string,
+            "operatorPwd": '',
+            "operatorUserId": '',
+            "validcode": '',
+        }
+        res = requests.post("http://210.77.16.21/eportal/InterFace.do?method=login", data=payload)
+        res.encoding = 'u8'
+    except (requests.exceptions.ConnectionError,
+            requests.exceptions.ConnectTimeout,
+            requests.exceptions.ReadTimeout):
+        return None
+    else:
+        return {
+            'result': res.json().get("result"),
+            'msg': res.json().get("message"),
+            'query_string': query_string
+        }
 
 
 def print_hi(name):
@@ -11,6 +40,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(login_wifi('2020E8013282037', '10741X'))
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
